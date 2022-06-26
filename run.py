1edd016eb737cb0bb5198ecbec884b2ad9a236ef
a747080030 <747080030@qq.com>
2022-06-26 20:36:15 +0800

    go1

--- run.py
+++ run.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
     # run test file
     # seldom.main("./test_dir/test_web_sample.py")
     # run test dir
-    seldom.main("./test_dir/test_moduleUi.py", browser="chrome", base_url="http://quick.testpub.cn", title="小叮当测试", tester="xiaodingdan", debug=True)
+    seldom.main("./test_dir/", browser="chrome", base_url="http://quick.testpub.cn", title="小叮当测试", tester="xiaodingdan", debug=False)
     # SeldomTestLoader.collectCaseInfo = True
     # main_extend = TestMainExtend(path="./test_dir/")
     # case_info = main_extend.collect_cases(json=True)
